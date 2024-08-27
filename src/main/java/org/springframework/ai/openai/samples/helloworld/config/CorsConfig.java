2021708cfc487768381ab695d9da664dbeff4708
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 13:45:47 +0900

    feat: CorsFilter 작성

---
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package org.springframework.ai.openai.samples.helloworld.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+}
